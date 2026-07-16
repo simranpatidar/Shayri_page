5a9317cfd2bd461478ee1114ad589ffb91d5cf85
simranpatidar <simran2005patidar@gmail.com>
2026-07-16 13:06:02 +0530

    commit

---
+++ shayri.js
@@ -0,0 +1,13 @@
+function text()
+{
+    let sim=document.getElementById("signin");
+    sim.classList.toggle("my");
+    let sim1=document.getElementsByTagName("main");
+    sim1[0].classList.toggle("my1");
+    let sim2=document.getElementsByClassName("nexta");
+    sim2[0].classList.toggle("my2");
+    let sim3=document.getElementsByTagName("header");
+    sim3[0].classList.toggle("my3");
+    let sim4=document.getElementsByClassName("sign");
+    sim4[0].classList.toggle("my4");
+}
